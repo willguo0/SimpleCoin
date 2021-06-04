a4febd8df8dd44dab00346fc3543884da409df20
dg314 <david_grossman@brown.edu>
2021-06-04 06:11:55 -0400

    test

--- pkg/blockchain/blockchain.go
+++ pkg/blockchain/blockchain.go
@@ -140,7 +140,7 @@ func (bc *Blockchain) Add(b *block.Block) {
 func (bc *Blockchain) Length() int {
 	bc.Lock()
 	defer bc.Unlock()
-	return bc.LastBlock.depth
+	return bc.LastBlock.depth + 1
 }
 
 // Get returns the blocks that corresponds to a
